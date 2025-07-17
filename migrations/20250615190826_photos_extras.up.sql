cf60003c34adacd6c6b681ab6f1c1b43b2cf19ff
Răzvan Filea <razvan.filea@gmail.com>
2025-07-17 12:07:26 +0300

    Keep only the first half of the SHA and store it as BLOB

--- migrations/20250615190826_photos_extras.up.sql
+++ migrations/20250615190826_photos_extras.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE photos_extras
 (
     id        INTEGER NOT NULL PRIMARY KEY,
-    sha       TEXT    NOT NULL,
+    hash      BLOB    NOT NULL,
     exif_json TEXT,
 
     FOREIGN KEY (id) REFERENCES photos (id) ON DELETE CASCADE
--- src/model/photo_extras.rs
+++ src/model/photo_extras.rs
@@ -2,6 +2,6 @@ use crate::model::exif_field::ExifFields;
 
 pub struct PhotoExtras {
     pub id: i64,
-    pub sha: String,
+    pub hash: Vec<u8>,
     pub exif_json: Option<sqlx::types::Json<ExifFields>>,
 }
