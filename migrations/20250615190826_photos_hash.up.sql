deb40f400857246e92ad871c6fd8f2646ccb2e0d
Răzvan Filea <razvan.filea@gmail.com>
2025-07-20 12:14:53 +0300

    Remove exif_data from photos_hash

--- migrations/20250615190826_photos_hash.up.sql
+++ migrations/20250615190826_photos_hash.up.sql
@@ -2,7 +2,6 @@ CREATE TABLE photos_hash
 (
     photo_id   INTEGER  NOT NULL PRIMARY KEY,
     hash       BLOB     NOT NULL,
-    exif_data  BLOB,
     created_at DATETIME NOT NULL DEFAULT current_timestamp,
 
     FOREIGN KEY (photo_id) REFERENCES photos (id) ON DELETE CASCADE
